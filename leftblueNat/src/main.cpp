846d6f0fba442c3f2a25bd1e95cc24f5afd2e51b
junnnaaaaaa <paxtonbennison@outlook.com>
2024-12-04 09:15:47 +0800

    added programs for other side, exact same as normal programs just reversed

---
+++ leftblueNat/src/main.cpp
@@ -0,0 +1,173 @@
+#include "main.h"
+#include "functions.hpp"
+#include "pros/misc.h"
+#pragma
+
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+  static bool pressed = false;
+  pressed = !pressed;
+  if (pressed) {
+    pros::lcd::set_text(2, "I was pressed!");
+  } else {
+    pros::lcd::clear_line(2);
+  }
+}
+//
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+  pros::lcd::initialize();
+  pros::lcd::set_text(1, "Hello noah! I am watching you ");
+  imu.reset(true);
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {
+  pros::delay(20);
+  mogoMech.retract();
+  pros::lcd::set_text(1, "auto initialized");
+  pidMove(880);
+  pidTurn(215);
+  movefb(-1.3, 100, 800);
+  mogoMech.toggle();
+  movelr(-0.1, 100, 200);
+  movefb(0.7, 600, 400);
+  pidTurn(90);
+  intake.move(200);
+  pros::delay(900);
+  mogoMech.toggle();
+  movefb(-0.4, 400, 500);
+  movefb(2.3, 400, 1000);
+  pidTurn(320);
+  movefb(-2.3, 100, 1000);
+  mogoMech.toggle();
+  pidTurn(270);
+  movefb(2, 300, 2000);
+  pidTurn(90);
+  movefb(4, 200, 2000);
+}
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connec:w
+ * ted, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+  std::string headingstr = "killing myself";
+  arm.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  bool intakeToggle = false;
+  bool canFlap = true;
+  bool canMogo = true;
+  bool canIntake = true;
+  std::string odostr = "i hate this";
+  while (true) {
+    // Arcade control scheme
+    int dir = master.get_analog(
+        ANALOG_LEFT_Y); // Gets amount forward/backward from left joystick
+    int turn = master.get_analog(
+        ANALOG_RIGHT_X);       // Gets the turn left/right from right joystick
+    left_mg.move(dir + turn);  // Sets left motor voltage
+    right_mg.move(dir - turn); // Sets right motor voltage
+    if (master.get_digital(
+            pros::E_CONTROLLER_DIGITAL_R2)) { // intake stuff with toggle
+      intakeToggle = false;
+      intake.move(-200);
+    } else if (master.get_digital(
+                   pros::E_CONTROLLER_DIGITAL_R1)) { // intake stuff with toggle
+      intakeToggle = false;
+      intake.move(200);
+    } else {
+      if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP) && canIntake) {
+        canIntake = false;
+        if (!intakeToggle) {
+          intakeToggle = true;
+        } else if (intakeToggle) {
+          intakeToggle = false;
+        }
+      } else if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+        canIntake = true;
+      }
+      if (intakeToggle) { // intake toggle basic logic
+        intake.move(200);
+      } else {
+        intake.move(0);
+      }
+    }
+    if (master.get_digital(
+            pros::E_CONTROLLER_DIGITAL_L1)) { // arm logic and stuff
+      arm.move(100);
+    } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+      arm.move(-100);
+    } else {
+      arm.move(0);
+    }
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) &&
+        canMogo) { // mogoMech toggle
+      mogoMech.toggle();
+      canMogo = false;
+    } else if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+      canMogo = true;
+    }
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B) &&
+        canFlap) { // mogoMech toggle
+      flap.toggle();
+      canFlap = false;
+    } else if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+      canFlap = true;
+    }
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+      odoRotation.reset_position();
+    }
+    headingstr = std::to_string(imu.get_heading());
+    odostr = std::to_string(odoRotation.get_position());
+    pros::lcd::set_text(2, headingstr);
+    pros::lcd::set_text(3, odostr);
+    pros::delay(20); // Run for 20 ms then update
+  }
+}
---
+++ rightBlueNats/src/functions.hpp
@@ -0,0 +1,136 @@
+#include "main.h"
+#include <cmath>
+#pragma once
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup
+    left_mg({-4, 3, -11}, pros::v5::MotorGears::blue,
+            pros::v5::MotorUnits::rotations); // Creates a motor group with
+// forwards
+pros::MotorGroup right_mg(
+    {8, -9, 19}, pros::v5::MotorGears::blue,
+    pros::v5::MotorUnits::rotations); // Creates a motor group with forwards
+// intake: 12
+pros::Motor intake(-12, pros::v5::MotorGears::green,
+                   pros::v5::MotorUnits::rotations);
+// arm: 13
+pros::Motor arm(-14, pros::v5::MotorGears::red,
+                pros::v5::MotorUnits::rotations);
+// mogomech: port g
+pros::adi::Pneumatics mogoMech('g', false, true);
+// flap: port h
+pros::adi::Pneumatics flap('h', false);
+// inertia moment: port 20
+pros::IMU imu(20);
+// me when i rotate: port 18
+pros::Rotation odoRotation(-18);
+void movefb(float amnt, int spd, int time) {
+  const double finalLeftPos = amnt - left_mg.get_position();
+  const double finalRightPos = amnt - right_mg.get_position();
+  left_mg.move_relative(amnt, spd);
+  right_mg.move_relative(amnt, spd);
+  pros::delay(time);
+}
+void movelr(float amnt, int spd, int wait) {
+  left_mg.move_relative(amnt, spd);
+  right_mg.move_relative(-amnt, spd);
+  pros::delay(wait);
+}
+void pidTurn(float targ) {
+  float error;
+  float inter = 0;
+  float preverror = 0;
+  float deriv;
+  float head;
+  float kp = 1.8;
+  float ki = 0.9;
+  float kd = 12.5;
+  float power = 100;
+  float dt = 20;
+  int count = 0;
+  std::string headstr = "kill me please";
+  std::string derivstr = "kill me please";
+  deriv = 2;
+  while (true) {
+    headstr = std::to_string(imu.get_heading());
+    head = imu.get_heading();
+    error = targ - head;
+    if (error > 180) {
+      error = error - 360;
+    }
+    if (error < -180) {
+    https: // www.vexrobotics.com/276-7417.html
+      error = error + 360;
+    }
+    if (error > -2 && error < 2) {
+      inter = error;
+    }
+    if (std::abs(inter) > 70) {
+      inter = 0;
+    }
+    inter += error;
+    deriv = error - preverror;
+    power = error * kp + inter * ki + deriv * kd;
+    left_mg.move(power);
+    right_mg.move(-power);
+    pros::lcd::set_text(2, headstr);
+    derivstr = std::to_string(deriv);
+    pros::lcd::set_text(3, derivstr);
+    derivstr = std::to_string(power);
+    pros::lcd::set_text(4, derivstr);
+    if (std::abs(error) < 1) {
+      count += 1;
+      if (count > 4) {
+        pros::lcd::set_text(4, "broken");
+        break;
+      }
+    }
+    preverror = error;
+    pros::delay(dt);
+  }
+  left_mg.move(0);
+  right_mg.move(0);
+
+  pros::lcd::set_text(5, "turn complete");
+}
+void pidMove(float targ) {
+  float error, deriv, amt, inter;
+  float preverror = 0;
+  float kp = 0.42;
+  float ki = 0.25;
+  float kd = 3;
+  float power = 100;
+  float dt = 20;
+  int count = 0;
+  odoRotation.reset_position();
+  std::string amtstr = "";
+  std::string errorstr = "";
+  while (true) {
+    amt = odoRotation.get_position();
+    amt = (amt / 36000) * 2.75 * M_PI * 2.54 * 10.0;
+    amtstr = std::to_string(amt);
+    error = targ - amt;
+    errorstr = std::to_string(error);
+    if (std::abs(inter) > 100 || std::abs(error) < 3) {
+      inter = 0;
+    }
+    if (std::abs(error) < 5) {
+      count++;
+      if (count > 3) {
+        break;
+      }
+    }
+    inter += error;
+    deriv = error - preverror;
+    power = (error * kp + inter * ki + deriv * kd) * 1;
+    left_mg.move(power);
+    right_mg.move(power);
+    preverror = error;
+    pros::lcd::set_text(2, amtstr);
+    pros::lcd::set_text(3, errorstr);
+    errorstr = std::to_string(power);
+    pros::lcd::set_text(4, errorstr);
+    pros::delay(dt);
+  }
+  left_mg.move(0);
+  right_mg.move(0);
+}
